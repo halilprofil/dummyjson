168170ecf60994801564a7954bb9a75f1e3431f4
eceylaan <ceylann.ece@gmail.com>
2024-09-05 19:15:15 +0300

    css added

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -3,6 +3,7 @@ import { fetchProductsByPage } from "../../services/products";
 import { useFetch } from "../../hooks/useFetch";
 import { Pagination } from "../pagination/Pagination";
 import { Spinner } from "../base/Spinner";
+import "./products.css";
 
 export const Products = () => {
   const [page, setPage] = useState(1);
@@ -11,7 +12,39 @@ export const Products = () => {
 
   return (
     <>
-      <div>{data && !loading ? data.products.map((product) => <li>{product.title}</li>) : !loading && "İçerik bulunamadı"}</div>
+      <div className="productsContainer">
+        {data && !loading
+          ? data.products.map((product) => (
+              <div className="productsCard" key={product.id}>
+                <img width={250} src={product.thumbnail} alt="" />
+                <p style={{ fontSize: 40 }}>
+                  <strong>${product.price}</strong>
+                </p>
+                <p>
+                  <strong>{product.title}</strong>
+                </p>
+                <p>{product.brand}</p>
+                <p>
+                  <strong>Point:</strong> {product.rating}
+                </p>
+                <div className="pRow">
+                  <p>
+                    <strong>Discount:</strong> %{product.discountPercentage}
+                  </p>
+                  <p>
+                    <strong>Stock:</strong> {product.stock}
+                  </p>
+                </div>
+                <div className="pRow">
+                  <p>
+                    <strong>Category:</strong> {product.category}
+                  </p>
+                  <div>{product.tags.map((tag) => `#${tag}`).join(" ")}</div>
+                </div>
+              </div>
+            ))
+          : !loading && "İçerik bulunamadı"}
+      </div>
       {loading && (
         <div>
           <Spinner />
